9e4d4a2e029f852c960eb4a658fece03c7f25291
Rustam2301 <62401714+Rustam2301@users.noreply.github.com>
2021-02-04 16:13:33 +0600

    Add files via upload

---
+++ public/loginPage.js
@@ -0,0 +1,12 @@
+'use strict'
+
+let thisUser = new UserForm;
+let err = new Error('Неверное имя пользователя или пароль');
+
+ thisUser.loginFormCallback = (data) => ApiConnector.login(data, (response)=>{
+    console.log(response);
+    if (response.succsess === true) location.reload()
+    else return err
+   });
+thisUser.registerFormCallback = (data) => ApiConnector.register(data, (response) =>
+location.reload());
